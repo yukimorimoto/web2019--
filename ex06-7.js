ebec62dd09670ffaee5b46c8f455cbe0206e1459
yukimorimoto <zm18075@josai.ac.jp>
2019-11-12 03:21:37 +0000

    11/12

---
+++ ex06-7.js
@@ -0,0 +1,7 @@
+var who='world';
+function sayhello67(){
+    alert('Hello,'+who+'!');
+}
+fumction someone(x){
+    who = x;
+}
--- ex6-5.js
+++ ex6-5.js
@@ -1,10 +1,11 @@
-var who='world';
-function sayhello(){
-    alert('Hello,'+who+'!');
+var who65='world';
+function sayhello65(){
+    alert('Hello,'+who65+'!');
 }
-function taro(){
-    who='太郎';
+
+function taro65(){
+    who65='太郎';
 }
-function hanako(){
-    who='花子';
+function hanako65(){
+    who65='花子';
 }
