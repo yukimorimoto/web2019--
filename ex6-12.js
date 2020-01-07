3bb666a9f5270d9cdf6a3678ee983e2a75ef9474
yukimorimoto <zm18075@josai.ac.jp>
2020-01-07 03:25:36 +0000

    1/07

---
+++ ex6-12.js
@@ -0,0 +1,6 @@
+function ThreeSecChange(){
+    //関数imgchange1()を3000ミリ秒間隔で呼び出す
+    setInterval("imgchange12()",3000);}
+    function imgchange12(){
+       document.getElementById('logo2').src ='./image/bulbon.gif';
+    }
